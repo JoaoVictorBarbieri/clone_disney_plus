198044932dc51fc5d6103f29c86141ed1fcc13df
João <joaov.b.euwest@gmail.com>
2024-04-24 09:19:46 -0300

    #create_sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-function testeGulp(cb) {
-    console.log ("Olá Mundo");
-    cb();
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function styles () {
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
 }
-exports.default = testeGulp;
+exports.default = styles;
+exports.watch = function(){
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
